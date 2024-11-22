ea1fcc58bea944e968a54284edbb8c836004ea02
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-11-22 02:27:38 -0300

    Añadir configuración de cors

--- src/main/java/com/techforb/challengemonitoreotechforbjava/config/AppConfig.java
+++ src/main/java/com/techforb/challengemonitoreotechforbjava/config/AppConfig.java
@@ -1,15 +1,21 @@
 package com.techforb.challengemonitoreotechforbjava.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class AppConfig {
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean(name = "modelMapper")
     public ModelMapper modelMapper() {
         return new ModelMapper();
@@ -20,16 +26,12 @@ public class AppConfig {
         return new BCryptPasswordEncoder();
     }
 
-    //cors
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("*").allowedHeaders("*");
             }
         };
     }
